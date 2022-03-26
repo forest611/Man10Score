7a63ee78b09f2d73af70a8bc694a33d458ed802e
forest611 <forest611328@gmail.com>
2022-03-26 22:56:45 +0900

    メニューを閉じた時に、キャッシュを削除していなかった問題を修正

--- src/main/java/red/man10/man10score/nick/NameColorMenu.kt
+++ src/main/java/red/man10/man10score/nick/NameColorMenu.kt
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.persistence.PersistentDataType
@@ -275,6 +276,11 @@ object NameColorMenu : Listener{
 
         }
 
+    }
+
+    @EventHandler
+    fun closeInventory(e:InventoryCloseEvent){
+        userCache.remove(e.player)
     }
 
 
